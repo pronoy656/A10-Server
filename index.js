9cd340e49a6cf16f9e795ba6a3cdf21dad52e529
pronoy656 <pronoypaul56@gmail.com>
2024-04-26 05:48:45 +0600

    send data from server to database

--- index.js
+++ index.js
@@ -28,12 +28,31 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const artAndCraftsCollection = client.db('artAndCraftsDB').collection('artAndCraftCollection')
+
+// Read
+app.get('/artAndCrafts',async(req,res) =>{
+    const cursor = artAndCraftsCollection.find();
+    const result = await cursor.toArray();
+    res.send(result);
+})
+
+// create
+  app.post('/artAndCrafts', async (req,res) =>{
+    const newData = req.body
+    console.log(newData)
+    const result = await artAndCraftsCollection.insertOne(newData);
+    res.send(result)
+  })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
