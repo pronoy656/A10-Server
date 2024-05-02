a635d00191647f217dcfcf49e424a1aa84489dd2
pronoy656 <pronoypaul56@gmail.com>
2024-05-02 17:39:34 +0600

    vercel deployment ready

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
+
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config();
+
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
 
 // add middleware
-app.use(cors());
+app.use(
+  cors({
+      origin: ['http://localhost:5173', 'https://assignment-10-7d341.web.app'],
+      credentials: true,
+  }),
+)
 app.use(express.json());
 
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.d5v1sm5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-console.log(uri)
+
 
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -26,8 +33,8 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+  
+   
 
     const artAndCraftsCollection = client.db('artAndCraftsDB').collection('artAndCraftCollection')
 
